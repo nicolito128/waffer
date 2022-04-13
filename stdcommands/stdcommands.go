bf9946f7231966259858931d6e11597e7f011b10
Nicolás Serna <nicolito128@hotmail.com>
2022-04-13 20:34:18 -0300

    Update docs

--- stdcommands/stdcommands.go
+++ stdcommands/stdcommands.go
@@ -16,6 +16,7 @@ func AddCommands() {
 	)
 }
 
+// HasCommand return true if command list has the command name passed.
 func HasCommand(commandName string) bool {
 	if commands.CommandList[commandName] != nil {
 		return true
@@ -24,6 +25,7 @@ func HasCommand(commandName string) bool {
 	return false
 }
 
+// GetCommandList return a map[string]:*WafferCommand
 func GetCommandList() commands.CList {
 	return commands.CommandList
 }
