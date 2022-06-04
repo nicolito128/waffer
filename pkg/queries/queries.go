ccfb9fb80b51b245c2df61897f3cfae77328ef69
Nicolás Serna <nicolito128@hotmail.com>
2022-06-04 14:26:47 -0300

    Update pkg queries

--- pkg/queries/queries.go
+++ pkg/queries/queries.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// Get performs a GET request to the specified URL and unmarshals the response into the specified interface.
 func Get(url string, target any) error {
 	res, err := http.Get(url)
 	if err != nil {
