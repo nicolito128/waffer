d26b71e634718a4f0ccd68145ad90c4ca2928844
Nicolás Serna <nicolito128@hotmail.com>
2022-07-25 03:50:49 -0300

    Update calc command

--- stdcommands/util/calc/calc.go
+++ stdcommands/util/calc/calc.go
@@ -1,14 +1,30 @@
 package calc
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/nicolito128/go-calculator"
+	"github.com/nicolito128/waffer/pkg/env"
 	"github.com/nicolito128/waffer/pkg/plugins"
 	"github.com/nicolito128/waffer/pkg/plugins/commands"
+	"github.com/nicolito128/waffer/pkg/plugins/commands/flags"
 	"github.com/nicolito128/waffer/pkg/plugins/supermessage"
 )
+
+var Arguments = []*flags.Token{
+	{
+		Name:        "f",
+		Optional:    true,
+		Description: "The number of decimal places to display the result.",
+	},
+	{
+		Name:        "op",
+		Optional:    false,
+		Description: "The operation to calculate.",
+	},
+}
 
 var Command = &commands.WafferCommand{
 	Plugin: &plugins.Plugin[*discordgo.MessageCreate]{
@@ -19,9 +35,9 @@ var Command = &commands.WafferCommand{
 
 	Data: &commands.CommandData{
 		Name:         "calc",
-		Description:  "A mathematical calculation command. Use the example: `waf!calculator (10 / 2) + 1`, `waf!calculator 4f (1 / 9)`. The argument `<number>f` sets how many decimal places to display the result. By default the command displays the first two decimal digits, but it also accepts: `0f`, `1f`, `2f`, `3f`, `4f`, `5f`, `6f` and just `f` (for all decimal digits).",
+		Description:  fmt.Sprintf("A mathematical calculation command. Ex: `%scalc --op (10 / 2) + 1`, `%scalc --f 4 --op (1 / 9)`. The `--f` flag sets how many decimal places to display the result. By default the command displays the first two decimal digits, but it also accepts: `0`, `1`, `2`, `3`, `4`, `5` and `6`.", env.Get("BOT_PREFIX"), env.Get("BOT_PREFIX")),
 		Category:     "util",
-		Arguments:    []string{"<number>f[optional]", "expression"},
+		Arguments:    strings.Split(flags.GetUsages(Arguments...), "\n"),
 		RequiredArgs: 1,
 		Permissions: &commands.CommandPermissions{
 			Require: discordgo.PermissionSendMessages,
@@ -32,39 +48,42 @@ var Command = &commands.WafferCommand{
 
 func Handler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	sm := supermessage.New(s, m)
-	args := sm.Arguments()
+	f := flags.ParseString(sm.PlainContent(), Arguments...)
 
-	var decimals string
-	if len(args) >= 2 {
-		if strings.HasSuffix(args[0], "f") {
-			decimals = args[0]
-			args = args[1:]
-		}
+	if !flags.ResolveFlags(f, Arguments) {
+		sm.ChannelSend(fmt.Sprintf("**Invalid flags**.\n```\n%s\n```", flags.GetUsages(Arguments...)))
+		return
 	}
 
-	content := strings.Join(args, "")
-	res, err := calculator.Resolve(content)
+	var operation = flags.GetFlag(f, "op").Value
+	var decimals string
+
+	if flags.HasFlag(f, "f") {
+		decimals = flags.GetFlag(f, "f").Value
+	} else {
+		decimals = "2"
+	}
+
+	res, err := calculator.Resolve(operation)
 	if err != nil {
 		sm.ChannelSend("Error: %s", err)
 		return
 	}
 
 	switch decimals {
-	case "0f":
-		sm.ChannelSend("`%s` = %.0f", content, res)
-	case "1f":
-		sm.ChannelSend("`%s` = %.1f", content, res)
-	case "3f":
-		sm.ChannelSend("`%s` = %.3f", content, res)
-	case "4f":
-		sm.ChannelSend("`%s` = %.4f", content, res)
-	case "5f":
-		sm.ChannelSend("`%s` = %.5f", content, res)
-	case "6f":
-		sm.ChannelSend("`%s` = %.6f", content, res)
-	case "f":
-		sm.ChannelSend("`%s` = %f", content, res)
+	case "0":
+		sm.ChannelSend("`%s` = %.0f", operation, res)
+	case "1":
+		sm.ChannelSend("`%s` = %.1f", operation, res)
+	case "3":
+		sm.ChannelSend("`%s` = %.3f", operation, res)
+	case "4":
+		sm.ChannelSend("`%s` = %.4f", operation, res)
+	case "5":
+		sm.ChannelSend("`%s` = %.5f", operation, res)
+	case "6":
+		sm.ChannelSend("`%s` = %.6f", operation, res)
 	default:
-		sm.ChannelSend("`%s` = %.2f", content, res)
+		sm.ChannelSend("`%s` = %.2f", operation, res)
 	}
 }
